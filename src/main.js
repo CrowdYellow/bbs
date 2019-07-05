73083991e9ce98a43d5d224c1d52c06af99211f1
CrowdYellow <echo61546@gmail.com>
2019-07-05 19:02:58 +0800

    安装mint ui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入全部组件
+import Mint from 'mint-ui';
+Vue.use(Mint);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   render: h => h(App)
-})
+});
