4ccc7c209cb7e9e863367b64a2af06469137a525
CrowdYellow <echo61546@gmail.com>
2019-07-05 19:41:43 +0800

    基础布局

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,29 @@
 <template>
   <div id="app">
-    <mt-header fixed title="固定在顶部"></mt-header>
+    <TheHeader/>
+    fdsgsdfgsdfgsdfg
+    <TheFooter/>
   </div>
 </template>
 
 <script>
+import TheHeader from '@/views/layouts/TheHeader'
+import TheFooter from '@/views/layouts/TheFooter'
 export default {
   name: 'App',
-  data () {
-    return{
-      list: [
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-      ]
-    }
+  components: {
+    TheHeader,
+    TheFooter,
   },
-  methods: {
-    loadMore() {
-      console.log("开始加载");
-      this.loading = true;
-      setTimeout(() => {
-        let last = this.list[this.list.length - 1];
-        for (let i = 1; i <= 10; i++) {
-          this.list.push(last + i);
-        }
-        this.loading = false;
-      }, 2500);
-    }
-  }
 }
 </script>
 
 <style lang="scss">
 $fa-font-path: "~font-awesome/fonts/";
 @import "~font-awesome/scss/font-awesome";
+@import "../static/css/reset.css";
+html,body,#app{
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/views/layouts/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <mt-tabbar v-model="selected" :fixed="true">
+    <mt-tab-item id="tab1">
+      首页
+    </mt-tab-item>
+    <mt-tab-item id="tab2">
+      动态
+    </mt-tab-item>
+    <mt-tab-item id="tab3">
+      消息
+    </mt-tab-item>
+    <mt-tab-item id="tab4">
+      我的
+    </mt-tab-item>
+  </mt-tabbar>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+  data () {
+    return {
+      selected: 'tab1'
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
