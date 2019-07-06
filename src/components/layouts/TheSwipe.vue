6b98a2a9f59db491fdaeb6c7ab5f6685384d96b0
CrowdYellow <echo61546@gmail.com>
2019-07-06 14:38:17 +0800

    轮播图

---
+++ src/components/layouts/TheSwipe.vue
@@ -0,0 +1,37 @@
+<template>
+  <mt-swipe :auto="4000" :default-index="defaultIndex" style="height: 10rem">
+    <mt-swipe-item
+      v-for="(item, index) in swipes"
+      :key="index"
+    ><img :src="item.path" alt="" style="width: 100%;height: 100%"></mt-swipe-item>
+  </mt-swipe>
+</template>
+
+<script>
+export default {
+  name: "TheSwipe",
+  data() {
+    return{
+      defaultIndex: 1,
+      swipes: [
+        {
+          url: '',
+          path: './static/images/banner/1.jpg',
+        },
+        {
+          url: '',
+          path: './static/images/banner/2.jpg',
+        },
+        {
+          url: '',
+          path: './static/images/banner/3.jpg',
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <TheSearch/>
     <TheTopNavs/>
+    <TheSwipe/>
     <mt-spinner type="triple-bounce" class="load" color="#06c1ae"></mt-spinner>
   </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import TheSearch from '@/components/layouts/TheSearch'
 import TheTopNavs from '@/components/layouts/TheTopNavs'
+import TheSwipe from '@/components/layouts/TheSwipe'
 export default {
   name: "Home",
   components: {
     TheSearch,
     TheTopNavs,
+    TheSwipe,
   }
 }
 </script>
